007beec74299d325ad9f772bc37797e9e2ace838
24sec <harnws@gmail.com>
2015-10-11 02:08:28 -0400

    this script now creates database and tables

--- install.php
+++ install.php
@@ -24,21 +24,53 @@
 		$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 		//$dbh->setAttributes(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$dbh->query("CREATE TABLE user (
+		$query = $dbh->prepare("CREATE TABLE user (
 			id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 			username VARCHAR(18) NOT NULL,
 			password VARCHAR(40) NOT NULL,
 			email VARCHAR(60),
 			reg_date TIMESTAMP
 			) COLLATE utf8_unicode_ci");
+		if( $query->execute() )
+			echo "Created table: user<br>";
+		else
+			echo "Failure to create table: user<br>";
 
-		$dbh->query("CREATE TABLE restaurant （
-			id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+		$query = $dbh->prepare("CREATE TABLE restaurant (
+			id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 			username VARCHAR(18) NOT NULL,
 			password VARCHAR(40) NOT NULL,
-			email VARCHAR(60),
-			reg_date TIMESTAMP
+			email VARCHAR(60) NOT NULL,
+			reg_date TIMESTAMP,
+			restaurant_name VARCHAR(60) NOT NULL
 			) COLLATE utf8_unicode_ci");
+		if( $query->execute() )
+			echo "Created table: restaurant<br>";
+		else
+			echo "Failure to create table: restaurant<br>";
+
+		$query = $dbh->prepare("CREATE TABLE menu (
+			id INT(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			owner_id INT(11) UNSIGNED NOT NULL,
+			menu_title VARCHAR(60) DEFAULT NULL,
+			create_date TIMESTAMP
+			) COLLATE utf8_unicode_ci");
+		if( $query->execute() )
+			echo "Created table: menu<br>";
+		else
+			echo "Failure to create table: menu<br>";
+
+		$query = $dbh->prepare("CREATE TABLE menu_item (
+			id INT(32) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			menu_id INT(12) UNSIGNED,
+			item_title VARCHAR(18) DEFAULT 'Undefined',
+			item_detail VARCHAR(128) DEFAULT 'Empty',
+			item_price DECIMAL(10,2)
+			) COLLATE utf8_unicode_ci");
+		if( $query->execute() )
+			echo "Created table: menu_item<br>";
+		else
+			echo "Failure to create table: menu_item<br>";
 	}catch(PDOException $e){
 		echo $e->getMessage();
 	}
