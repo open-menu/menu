beb687828ce7c9d9503ba19bd34c9b1e90248ac3
eledebaozi <jdu@buffalo.edu>
2015-10-28 01:01:45 -0400

    user-page

---
+++ js/user.js
@@ -0,0 +1,11 @@
+function MM_over(mmObj) {
+	var mSubObj = mmObj.getElementsByTagName("div")[0];
+	mSubObj.style.display = "block";
+	mSubObj.style.backgroundColor = "white";
+	mSubObj.style.left = ;
+}
+function MM_out(mmObj) {
+	var mSubObj = mmObj.getElementsByTagName("div")[0];
+	mSubObj.style.display = "none";
+	
+}
--- mn-user.php
+++ mn-user.php
@@ -1,4 +1,5 @@
 
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -9,115 +10,190 @@
 	<title>草泥马！！草泥马的个人主页</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-  <meta name="viewport" content="width=device-width" />
-  <!--    个人主页navbar   -->
-  <link href="css/bootstrap.css" rel="stylesheet" />
-  <link href="css/gsdk.css" rel="stylesheet"/>
-  <link href="css/demo.css" rel="stylesheet" />
-  <link href="css/user.css" rel="stylesheet" />
+	<meta name="viewport" content="width=device-width" />
+	<!--    个人主页navbar   -->
+	<link href="css/bootstrap.css" rel="stylesheet" />
+	<link href="css/gsdk.css" rel="stylesheet"/>
+	<link href="css/demo.css" rel="stylesheet" />
+	<link href="css/user.css" rel="stylesheet" />
 
-  <!--     Fonts and icons     -->
-  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-  <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
-  <link href="css/pe-icon-7-stroke.css" rel="stylesheet" />
-  <link href="css/site.css" rel="stylesheet" />
-  <script type="text/javascript" src="user.js"></script>
+	<!--     Fonts and icons     -->
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+	<link href="css/pe-icon-7-stroke.css" rel="stylesheet" />
+	<link href="css/site.css" rel="stylesheet" />
 </head>
 
 <body>
-	<!-- 头部 -->
-	<div>
-		<nav class="navbar navbar-default">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="layout-user-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-default2">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Home</a>
-      </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="navigation-default2">
-        <ul class="nav navbar-nav">
-          <!-- Personal Favorite Button-->
-          <li>
-            <button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-              草泥马个人收藏
-            </button>
-          </li>
-          <!-- Restaurant Data Button -->
-          <li>
-            <button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-              草泥马餐馆资料
-            </button>
-          </li>
-          <li>
-            <button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-              草泥马个人分享
-            </button>
-          </li>
-        </ul>
-        <!--   Sign out Button -->
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-              <i class="fa fa-sign-in"></i>
-              草泥马Sign out
-            </button>
-          </li>
-        </ul>
+	<div class="wrapper">
+		<div id="user-container">
+			<!-- 头部 -->
+			<div>
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="layout-user-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-default2">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Home</a>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="">
+						<ul class="nav navbar-nav">
+							<!-- Personal Favorite Button-->
+							<li>
+								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
+									草泥马个人收藏
+								</button>
+							</li>
+							<!-- Restaurant Data Button -->
+							<li>
+								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
+									草泥马餐馆资料
+								</button>
+							</li>
+							<li>
+								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
+									草泥马个人分享
+								</button>
+							</li>
+						</ul>
+						<!--   Sign out Button -->
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
+									<i class="fa fa-sign-in"></i>
+									草泥马Sign out
+								</button>
+							</li>
+						</ul>
 
-      </div><!-- /.navbar-collapse -->
-    </nav>
+					</div><!-- /.navbar-collapse -->
+				</nav>
+				<!--    整体板块     -->
 
-<!--    整体板块     -->
-  <div class="content-mu">
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    <div class="rectangle-le-box"></div>
-    <div class="rectangleve2-box"></div>
-    <div class="rectangle-ve-box"></div>
-    <div class="rectangle-ve-box"></div>
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    <div class="sq-box-mu">    </div>
-    .
+				<!-- First row  推销-->
+				<div class="content-mu">
+					<div class="row" width="100%" max-height="33.33%">
+						
+						<div class="foryou-mu"> 
+
+							<a href="#" class="user-tooltip" >
+								<img src="blog_post.jpg" width="150px"; height="150px">
+								<span class="user-classic">
+									王尼玛餐馆
+									<p>地址</p>
+									<p>电话</p>
+									<p>主页</p>
+								</span>
+							</a>
+							<div class="sq-box">
+								<div class="sq-name-box">
+									韩国石锅海鲜饭
+								</div>
+								<div class="sq-name-box" style="background-color:green">
+									第二行草泥马
+								</div>
+								<div class="sq-name-box" style="background-color:red">
+									第三行草泥马
+								</div>
+								<div class="sq-name-box" style="background-color:blue">
+									第四行草泥马
+								</div>
+								<div class="sq-name-box" style="background-color:white">
+									第五行草泥马
+								</div>
+							</div>
+						</div>
+						
+						<div class="foryou-mu">  
+							<a href="#" class="user-tooltip" >
+								<img src="blog_post.jpg" width="150px"; height="150px">
+								<span class="user-classic">
+									<img src="blog_post.jpg" style="width:100px; height:100px; border-radius:50%; overflow:hidden;">
+										王尼玛餐馆
+									<p>地址</p>
+									<p>电话</p>
+									<p>主页</p>
+								</span>
+							</a>
+						</div>
+						<div class="foryou-mu">  
+							<a href="#" class="user-tooltip" >
+								<img src="blog_post.jpg" width="150px"; height="150px">
+								<span class="user-classic" >
+									王尼玛餐馆
+									<p>地址</p>
+									<p>电话</p>
+									<p>主页</p>
+								</span>
+							</a>
+						</div><div class="foryou-mu">  
+						<a href="#" class="user-tooltip">
+							<img src="blog_post.jpg" width="150px"; height="150px">
+							<span>
+								草泥马
+							</span>
+						</a>
+					</div>
+
+				</div>
+
+				<!-- second row  个人  -->
+				<!-- 历史栏  -->
+				<div class="row" width="100%" max-height="33.33%">
+					<div class="history-box">
+						<h3>历史</h3>
+						<li>
+
+						</li>					
+					</div>
+					<!--  主菜单  -->
+					<div class="main-box">
+						<div class="user-info">
+							<div class="container">
+								<a href="#" class="food-catagory">
+									<img src="blog_post.jpg" class="path1">
+									<span>
+										<div class="btn-food-catagory0"><strong><p>草泥马是傻瓜</p></strong></div>
+										<div class="btn-food-catagory1"><strong><p>草泥马是笨蛋</p></strong></div>
+										<div class="btn-food-catagory2"><strong><p>草泥马是蠢材</p></strong></div>
+										<div class="btn-food-catagory3"><strong><p>草泥马是屁屁</p></strong></div>
+										<div class="btn-food-catagory4"><strong><p>草泥马去吃饭</p></strong></div>
+										<div class="btn-food-catagory5"><strong><p>草泥马去洗澡</p></strong></div>
+									</span>
+								</a>
+							</div>
+
+						</div>
+					</div>
+
+
+					<!--  未知box  -->
+					<div class="rectangle-ve-box"></div>
+					<div class="rectangle-ve-box">
+						<a href="#" class="user-tooltip">你在干嘛
+							<span>
+								what are you 弄啥咧
+								<strong><p>王尼玛打</p><p>草泥马</p></strong>
+							
+							</span>
+						</a>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  </div>
 </body>
+
+
+
+
+
 </html>
-=======
-<?php
-	echo "I'm a user";
-?>
->>>>>>> master
