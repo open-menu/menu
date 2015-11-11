5a33bee73d362f53b1796cbb0e5a617d4e150bae
eledebaozi <jdu@buffalo.edu>
2015-11-10 23:42:40 -0500

    user-page

--- mn-user.php
+++ mn-user.php
@@ -5,7 +5,7 @@
 	<link rel="icon" type="image/png" href="img/favicon.ico">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>草泥马啊！！！！</title>
+	<title>NewBee Restaurant Personal Page</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	<meta name="viewport" content="width=device-width" />
@@ -14,6 +14,7 @@
 	<link href="css/gsdk.css" rel="stylesheet"/>
 	<link href="css/user.css" rel="stylesheet" />	
 	<link href="css/demo.css" rel="stylesheet" />
+	<link href="css/datouwang.css" rel="stylesheet" type="text/css">
 
 	<!--     Fonts and icons     -->
 	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
@@ -23,274 +24,228 @@
 	<link href="css/site.css" rel="stylesheet" />
 </head>
 <body>
-
-
 	<div class="parallax">
-		<div class="parallax-image">
-			
-		</div>
-		<div>
-			<!-- Navbar will come here -->
-			<nav class="navbar navbar-default" role="navigation-demo">
+		<div class=" backgroundimage" style="background-repeat:repeat">
+			<div>
+				<!-- Navbar will come here -->
+				<nav class="navbar navbar-default" role="navigation-demo">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-default2">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Home</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="navigation-default2">
+
+							<ul class="nav navbar-nav">
+								<!-- Restaurant Data Button -->
+								<li>
+									<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
+										<i class="fa fa-book"></i>Restaurants
+									</button>
+								</li>
+								<li>
+									<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
+										<i class="fa fa-share-alt"></i>Share
+									</button>
+								</li>
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li>
+									<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
+										<i class="fa fa-sign-in"></i>
+										Sign out
+									</button>
+								</li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav><!-- end navbar -->
+
+				<!--    Body     -->
 				<div class="container">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-default2">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="#">Home</a>
+					<!-- First row  Sales-->
+					<div class="row" style="margin-top:-3.8em; margin-bottom:-1em" >
+						<h4>This week</h4>
 					</div>
-
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="navigation-default2">
-
-						<ul class="nav navbar-nav">
-							<li>
-								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-									草泥马个人收藏
-								</button>
-							</li>
-							<!-- Restaurant Data Button -->
-							<li>
-								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-									草泥马餐馆资料
-								</button>
-							</li>
-							<li>
-								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-									草泥马个人分享
-								</button>
-							</li>
-						</ul>
-						<ul class="nav navbar-nav navbar-right">
-							<li>
-								<button class="btn btn-round btn-fill btn-info" data-toggle="" data-target="">
-									<i class="fa fa-sign-in"></i>
-									草泥马Sign out
-								</button>
-							</li>
-						</ul>
-
-					</div><!-- /.navbar-collapse -->
-				</div>
-
-			</div><!-- /.container-fluid -->
-
-		</nav><!-- end navbar -->
-
-
-
-		<!--    整体板块     -->
-
-		
-		<div class="container">
-			<!-- First row  推销-->
-			<div class="row" width="100%" max-height="33.33%">
-				<div class="foryou-mu"> 
-
-					<a href="#" class="user-tooltip" >
-						<img src="blog_post.jpg" width="155"; height="173">
-						<span>
-							<img src="../menu/image/2.jpg" style="width:100px; height:100px; border-radius:50%; overflow:hidden;">
-							王尼玛餐馆
-							<p>地址</p>
-							<p>电话</p>
-							<p>主页</p>
-						</span>
-					</a>
-					<div class="sq-box">
-						<div class="sq-name-box">
-							石锅海鲜饭
-						</div>
-						<p></p>Priace: <i class="fa fa-usd"></i><i class="fa fa-usd"></i>
-						<p></p><i class="fa fa-star" align="center"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
-					</div>
-				</div>
-				<div class="foryou-mu">  
-					<a href="#" class="user-tooltip" >
-						<img src="../menu/image/1.jpg" width="155"; height="173">
-						<span>
-							<img src="../menu/image/2.jpg" style="width:100px; height:100px; border-radius:50%; overflow:hidden;">
-							王尼玛餐馆
-							<p>地址</p>
-							<p>电话</p>
-							<p>主页</p>
-						</span>
-					</a>
-					<div class="sq-box">
-						<div class="sq-name-box">
-							巨蟹机器人
-						</div>
-						<p></p>Priace: <i class="fa fa-usd"></i><i class="fa fa-usd"></i>
-						<p></p><i class="fa fa-star" align="center"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
-					</div>
-				</div>
-				<div class="foryou-mu">  
-					<a href="#" class="user-tooltip" >
-						<img src="../menu/image/4.jpg" width="155"; height="173">
-						<span>
-							<img src="../menu/image/2.jpg" style="width:100px; height:100px; border-radius:50%; overflow:hidden;">
-							鱿鱼杂锦烧
-							<p>地址</p>
-							<p>电话</p>
-							<p>主页</p>
-						</span>
-					</a>
-					<div class="sq-box">
-						<div class="sq-name-box">
-							鱿鱼杂锦烧
-						</div>
-						<p></p>Priace: <i class="fa fa-usd"></i><i class="fa fa-usd"></i>
-						<p></p><i class="fa fa-star" align="center"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
-					</div>
-				</div>
-				<div class="foryou-mu">  
-					<a href="#" class="user-tooltip">
-						<img src="../menu/image/7.jpg" width="155"; height="173">
-						<span>
-							<img src="../menu/image/2.jpg" style="width:100px; height:100px; border-radius:50%; overflow:hidden;">
-							王尼玛餐馆
-							<p>地址</p>
-							<p>电话</p>
-							<p>主页</p>
-						</span>
-					</a>
-					<div class="sq-box">
-						<div class="sq-name-box">
-							特色香蕉抹茶窜
-						</div>
-						<p></p>Priace: <i class="fa fa-usd"></i><i class="fa fa-usd"></i>
-						<p></p><i class="fa fa-star" align="center"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
-					</div>
-
-				</div>
-
-				<!-- second row  个人  -->
-				<!-- 历史栏  -->
-				<div class="row" width="100%" max-height="33.33%">
-					<!--  主菜单  -->
-					<div class="main-box">
-						<div class="user-info">
-							<a href="#" class="food-catagory">
-								<img src="../menu/image/2.jpg" class="path1">
-								<span>
-									<div class="btn-food-catagory0" align="center"><p></p><strong>草泥马中餐</strong></div>
-									<div class="btn-food-catagory1" align="center"><p></p><strong>草泥马美国</strong></div>
-									<div class="btn-food-catagory2" align="center"><p></p><strong>草泥马韩国</strong></div>
-									<div class="btn-food-catagory3" align="center"><p></p><strong>草泥马日本</strong></div>
-									<div class="btn-food-catagory4" align="center"><p></p><strong>草泥马越南</strong></div>
-									<div class="btn-food-catagory5" align="center"><p></p><strong>更多</strong></div>
-								</span>
-							</a>
-							<div class="btn-ice1" align="center">
-								<p></p><strong>棒棒草泥马</strong>
-							</div>
-							<div class="btn-ice2" align="center">
-								<p></p><i class="fa fa-cog fa=spin"></i><strong>账号管理</strong>
-							</div>
-							<div class="btn-ice3" align="center">
-								<p></p><strong>账号管理</strong>
-							</div>
-							<div class="btn-ice4">
-
-							</div>
-
-						</div>
-					</div>
-					<!--   中间盒子   -->
-					<div class="main-box">
-						<div class="housetop-box">	
-							<div id="house-box"></div>
-							<div id="house-box2"></div>
-							<div id="house-box3"></div>
-							<div class="newbee" align="center">
-								<p></p><P></P><strong>NewBee</strong><p>Logo</p>
-							</div>
-						</div>
-						<div class="housetop-box2">
-
-							<div class="house-favo-box">
-								<a href="#" class="fav-list">
-
-									<div class="house-favo" align="center">
-										<p></p><strong>个人收藏</strong>
-										<div class="winleft">
-											<div class="win-line1"></div>
-											<div class="win-line2"></div>
-										</div>
+					<div class="row">
+						<div class = " col-md-3 col-md-3">
+							<a href ="#" class = "thumbnail" >
+								<img src ="blog_post.jpg" alt = "..."></a>
+								<div class = "caption" style="margin-top:-1em">             
+									<h5> Restaurant Name1</h5>
+									<div class = "caption" style="margin-top:-1em">
+										<h5> price: $$ <br> 
+											<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>
+										</h5>
 									</div>
+								</div>
+						</div>
+
+						<div class = " col-md-3 col-md-3">
+							<a href ="#" class = "thumbnail" >
+								<img src ="blog_post.jpg" alt = "..."></a>
+								<div class = "caption" style="margin-top:-1em">             
+									<h5> Restaurant Name1</h5>
+									<div class = "caption" style="margin-top:-1em">
+										<h5> price: $$ <br> 
+											<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>
+										</h5>
+									</div>
+								</div>
+						</div>
+					
+						<div class = " col-md-3 col-md-3">
+							<a href ="#" class = "thumbnail" >
+								<img src ="blog_post.jpg" alt = "..."></a>
+								<div class = "caption" style="margin-top:-1em">             
+									<h5> Restaurant Name1</h5>
+									<div class = "caption" style="margin-top:-1em">
+										<h5> price: $$ <br> 
+											<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>
+										</h5>
+									</div>
+								</div>
+						</div>
+
+						<div class = "col-md-3 col-md-3">
+							<a href ="#" class = "thumbnail" >
+								<img src ="blog_post.jpg" alt = "..."></a>
+								<div class = "caption" style="margin-top:-1em">             
+									<h5> Restaurant Name1</h5>
+									<div class = "caption" style="margin-top:-1em">
+										<h5> price: $$ <br> 
+											<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>
+										</h5>
+									</div>
+								</div>
+						</div>
+					</div> <!-- End first row -->
+
+			<!-- second row  personal  -->
+					<div class="row">
+				<!--  Personal setting  -->
+						<div class="col-md-3 col-md-3" style="margin-top:1em">
+							<div class="user-info">
+								<a href="#" class="food-catagory">
+									<img src="../menu/image/2.jpg" class="path1">
 									<span>
-										<div class="fav-detal-box">
-											<ul>
-												<li>asdasd</li>
-												<li>草泥马啊</li>
-											</ul>
-										</div>
+										<div class="btn-food-catagory0" align="center"><p></p><i class="icon-hamburger"></i><strong>American</strong></div>
+										<div class="btn-food-catagory1" align="center"><p></p><strong>Chinese</strong></div>
+										<div class="btn-food-catagory2" align="center"><p></p><strong>Korean</strong></div>
+										<div class="btn-food-catagory3" align="center"><p></p><strong>Japanese</strong></div>
+										<div class="btn-food-catagory4" align="center"><p></p><strong>Indian</strong></div>
+										<div class="btn-food-catagory5" align="center"><p></p><strong>More</strong></div>
 									</span>
 								</a>
-							</div>
 
-
-							<div class="house-wish-box">
-								<a href="#" class="wish-list">
-									<div class="house-wish" align="center">
-										<p></p><strong>愿望盒</strong>
-										<div class="winright">
-											<div class="win-line1"></div>
-											<div class="win-line2"></div>
-										</div>										
+								<div class="btn-ice1" align="center"><p></p><strong>Names</strong></div>
+								<div class="btn-ice2" align="center"><p></p><i class="fa fa-cog fa=spin"></i><strong>Account</strong></div>
+								<div class="btn-ice3" align="center"><p></p><i class="fa fa-shopping-cart"></i><strong>Cart</strong></div>
+								<div class="btn-ice4">
+						<!-- day setting -->
+									<div class="day-box" style="margin-top:-17em; margin-left:10em" >
+										<h2>Nov.</h2>
+										<p></p><h3>10</h3>
 									</div>
-									<span>
-										<div class="wish-detal-box">
-											<div class="btn-ice1">想吃什么呢</div>
-											<div class="btn-ice1"></div>
-											<div class="btn-ice1"></div>
-											<div class="btn-ice1"></div>
-											<div class="btn-ice1"></div>
-										</div>
-									</span>
-								</a>
-								
+								</div>
 							</div>
-							<div id="house-box4"></div>
-							<div id="house-box5"></div>
-							<div id="house-box6"></div>
-							<div class="mouth"></div>
-
-						</div>	<!--housebox2 end -->
-					</div>
-					<div class="history-box">
-
-					</div>
-
-				</div>
-			</div>
-		</div>
-	</div>
-	
-</div>
-
-
-
-
-
-
-
-
-
-
-
-</div>
-
-<!-- Modal -->
-
-
-
-
+				<!-- wather：sunny cloudy rainy snowy starry stormy -->
+							<div class="cloudy"></div>
+						</div>
+				<!--   House   -->
+						<div class="col-md-5 col-md-5" style="margin-left:-0.5em; margin-top:-1em">
+							<div class="housetop-box">	
+								<div id="house-box"></div>
+								<div id="house-box2"></div>
+								<div id="house-box3"></div>
+								<div class="newbee" align="center"><p></p><P></P><strong>NewBee</strong><p>Logo</p></div>
+							</div>
+							<div class="housetop-box2">
+								<div class="house-favo-box">
+									<a href="#" class="fav-list">
+										<div class="house-favo" align="center">
+											<p></p><i class="fa fa-heart"></i><strong>Favorites</strong>
+											<span>
+												<div class="fav-detal-box">
+													<div align="center"><h3>Favoritess</h3></div>
+													<ul>
+														<li></li>
+														<li></li>
+														<li></li>
+														<li></li>
+													</ul>
+												</div>
+											</span>
+											<div class="winleft">
+												<div class="win-line1"></div>
+												<div class="win-line2"></div>
+											</div>
+										</div>
+									</a>
+								</div>
+								<div class="house-coupon-box">
+									<a href="#" class="coupon-list">
+										<div class="house-coupon" align="center">
+											<p></p><i class="fa fa-calendar"></i><strong>Coupon</strong>
+											<div class="winright">
+												<div class="win-line1"></div>
+												<div class="win-line2"></div>
+											</div>										
+										</div>
+										<span>
+											<div class="coupon-detal-box">
+												<div align="center"><h3>Coupons</h3></div>
+												<ul>
+													<li></li>
+													<li></li>
+													<li></li>
+													<li></li>
+												</ul>
+											</div>
+										</span>
+									</a>
+								</div>
+								<div id="house-box4"></div>
+								<div id="house-box5"></div>
+								<div id="house-box6"></div>
+								<div class="mouth"></div>
+							</div>
+						</div><!-- House end -->	
+						<div class="col-md-4 col-md-4" >
+								<div class="parking1" align="center">
+									<div id="parking4"></div>
+									<div id="parking2"></div>
+									<div id="parking3"></div>
+									<h3><i class="fa fa-history"></i>  Parking...</h3>
+									<div class="parking5" align="left">
+										<span class="windmill2">
+								         	<span class="pillar">
+								         		<h1><i class="fa fa-home" style="margin-left:0.6em"></i></h1>
+								         	</span>
+								         	<span class="axis">
+								       			<span class="swing"></span>
+								         		<span class="swing2"></span>
+								         		<span class="swing3"></span>
+								         	</span>
+								         </span>
+									</div>					
+								</div>
+								<div><img src="../menu/image/3.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-bottom:0.5em"></div>
+								<div><img src="../menu/image/4.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-left:5em; margin-bottom:0.5em"></div>
+								<div><img src="../menu/image/6.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-left:10em; margin-bottom:0.5em"></div>
+						</div>
+					</div>	<!-- Send row end -->
+				</div><!-- container end -->
+			</div><!-- End div -->
+		</div> <!-- End background -->
+	</div> <!-- parallax end -->
 </body>
-
-
 </html>
