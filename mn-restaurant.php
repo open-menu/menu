8fc3e6af979718fe93658c7afb4e5325dad3bec2
24sec <harnws@gmail.com>
2015-12-09 14:12:31 -0500

    replaced restaurant with restaurant-profile

--- mn-restaurant.php
+++ mn-restaurant.php
@@ -19,59 +19,169 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta charset="utf-8" />
+  <link rel="icon" type="image/png" href="img/favicon.ico">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>GridMenu</title>
+  <title>GridMenu</title>
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
     <link href="css/bootstrap.css" rel="stylesheet" />
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/gsdk.css" rel="stylesheet"/>
-    <link href="css/gsdk-base.css" rel="stylesheet" />
-
+    <link href="css/animate.min.css" rel="stylesheet"/>
+    
+     <link href="css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+    
     <link href="css/demo.css" rel="stylesheet" />
 
     <!--     Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="css/pe-icon-7-stroke.css" rel="stylesheet" />
     <link href="css/site.css" rel="stylesheet" />
 </head>
-<body>
-<h1> Restaurant </h1>
-<a href="mn-create-menu.php">Create Menu</a><br>
-<a href="mn-add-item.php">Add Item</a>
 
+        </div>
+          <div>
+            <!-- Navbar will come here -->
+               <nav class="navbar navbar-default" role="navigation-demo">
+                  <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-default2">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="../menu">Home</a>
+                    </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="navigation-default2">
+                      <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                TeamNewbee
+                                <b class="caret"></b>
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a href="http://teamnewbee.blogspot.com/">About us</a></li>
+                                <li class="divider"></li>
+                                <li><a href="https://github.com/open-menu/menu">Github Repo</a></li>
+                                <li class="divider"></li>
+                                <li><a href="http://teamnewbee.blogspot.com/2015/09/project-pitch.html">Project Pitch</a></li>
+            
+                              </ul>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" data-toggle="search" class="hidden-xs"><i class="fa fa-search"></i></a>
+                        </li>
+                      </ul>
+                       <form class="navbar-form navbar-left navbar-search-form" role="search">
+                         <div class="form-group">
+                              <input type="text" value="" class="form-control" placeholder="Search for a restaurant">
+                         </div>
+                      </form>
+                      <ul class="nav navbar-nav navbar-right">
+                            <li><button class="btn btn-round btn-fill btn-info" data-toggle="modal" onclick="window.location.href='mn-signout.php'"><i class="fa fa-sign-in"></i>&nbsp&nbspSign Out</button></li>
+                       </ul>
+
+                    </div><!-- /.navbar-collapse -->
+                  </div><!-- /.container-fluid -->
+                 </nav>
+                 <!-- end navbar -->
+             </div>
+
+<div class="wrapper">
+     <div class="container">
+           <div class="sidebar" data-color="orange">    
+           <div class="sidebar-wrapper">
+            <div class="logo">
+                <a class="simple-text">
+                    Grid Menu
+                </a>
+            </div>                       
+            <ul class="nav">
+                <li class="active">
+                    <a href="#">
+                        <!--<i class="pe-7s-user"></i> -->
+                        <p>Profile</p>
+                    </a>
+                </li> 
+                <li>
+                    <a href="#">
+                        <!--<i class="pe-7s-note2"></i> -->
+                        <p>Menu List</p>
+                    </a>        
+                </li>
+                <li>
+                    <a href="maps.html">
+                        <!--<i class="pe-7s-map-marker"></i> -->
+                        <p>Maps</p>
+                    </a>        
+                </li>
+            </ul> 
+      </div>
+    </div>
+       <div class="main-panel">
+          
+                            <div class="image">
+
+                            </div>
+                            <div class="content">
+                                <div class="author">
+                                     <a href="#">
+                                    <img id="restaurant image"class=" col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3 text-center" src="image/newbee.png" alt="..." >                       
+                                      <h4 class="name text-center col-md-2 col-md-offset-5 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">#restaurant name#<br />
+                                         <small>#restaurant address#</small>
+                                      </h4> 
+                                    </a>
+                                </div>  
+                                <p class="description text-center col-md-2 col-md-offset-5 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3"> #restaurant profile data# 
+                                </p>
+                            </div>
+                            <hr>
+                            <div class="text-center col-md-offset-5 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+                                <button href="#" class="btn btn-simple"><i class="fa fa-facebook-square"></i></button>
+                                <button href="#" class="btn btn-simple"><i class="fa fa-twitter"></i></button>
+                                <button href="#" class="btn btn-simple"><i class="fa fa-google-plus-square"></i></button>
+    
       
-
-</body>
-
-<script src="js/jquery-1.10.2.js" type="text/javascript"></script>
-
-	<script src="js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
-	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+                        </div>
+                    </div>
+             
+                </div>                    
+            </div>
+        </div>
     
-	<!--  Plugins -->
-	<script src="js/jquery.bootstrap.wizard.js" type="text/javascript"></script>
-	<script src="js/gsdk-checkbox.js"></script>
+     </div>
+     
+</div>
+
+
+
+    <script src="js/jquery-1.10.2.js" type="text/javascript"></script>
+  <script src="js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.js" type="text/javascript"></script>
+
+  <!--  Plugins -->
+  <script src="js/gsdk-checkbox.js"></script>
     <script src="js/site.js"></script>
-	<script src="js/gsdk-morphing.js"></script>
-	<script src="js/gsdk-radio.js"></script>
-	<script src="js/gsdk-bootstrapswitch.js"></script>
-	<script src="js/bootstrap-select.js"></script>
-	<script src="js/bootstrap-datepicker.js"></script>
-	<script src="js/chartist.min.js"></script>
+  <script src="js/gsdk-morphing.js"></script>
+  <script src="js/gsdk-radio.js"></script>
+  <script src="js/gsdk-bootstrapswitch.js"></script>
+  <script src="js/bootstrap-select.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/chartist.min.js"></script>
     <script src="js/jquery.tagsinput.js"></script>
-    
-
-	<script src="js/get-shit-done.js"></script>
-    <script src="js/jquery.validate.min.js"></script>
-    <script src="js/wizard.js"></script>
-
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+  <script src="js/get-shit-done.js"></script>
+
 </html>
