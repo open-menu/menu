bb888a557c0c13a26d746592a3391cc0ffb3e0d0
24sec <harnws@gmail.com>
2015-12-10 11:38:24 -0500

    Revert "Merge pull request #15 from open-menu/jdu"
    This reverts commit ab8ec5fcdb0b067bed9b7ed53d68ec8be8848a59, reversing
    changes made to 10f6d1b26dbddec57a3c42e5ca716d5044a16938.

--- mn-user.php
+++ mn-user.php
@@ -84,13 +84,12 @@
 				<!--    Body     -->
 				<div class="container">
 					<!-- First row  Sales-->
-                    <div class="row" style="font-size:30px; margin:-15px 0 20px 0;border-bottom:1px solid black;"> This Week</div>
+					<div class="row" style="margin-top:-3.8em; margin-bottom:-1em" >
+						<h4>This week</h4>
+					</div>
 					<div class="row">
-
-		<!-- 需要菜的图片，名字，价钱 -->
-
 						<div class = " col-md-3 col-md-3">
-							<a href ="mn-dish-detail.php" class = "thumbnail" >
+							<a href ="#" class = "thumbnail" >
 								<img src ="../menu/image/unknow-img.png" alt = "..."></a>
 								<div class = "caption" style="margin-top:-1em">             
 									<h5> Restaurant Name1</h5>
@@ -103,7 +102,7 @@
 						</div>
 
 						<div class = " col-md-3 col-md-3">
-							<a href ="mn-dish-detail.php" class = "thumbnail" >
+							<a href ="#" class = "thumbnail" >
 								<img src ="blog_post.jpg" alt = "..."></a>
 								<div class = "caption" style="margin-top:-1em">             
 									<h5> Restaurant Name1</h5>
@@ -116,7 +115,7 @@
 						</div>
 					
 						<div class = " col-md-3 col-md-3">
-							<a href ="mn-dish-detail.php" class = "thumbnail" >
+							<a href ="#" class = "thumbnail" >
 								<img src ="blog_post.jpg" alt = "..."></a>
 								<div class = "caption" style="margin-top:-1em">             
 									<h5> Restaurant Name1</h5>
@@ -129,7 +128,7 @@
 						</div>
 
 						<div class = "col-md-3 col-md-3">
-							<a href ="mn-dish-detail.php" class = "thumbnail" >
+							<a href ="#" class = "thumbnail" >
 								<img src ="blog_post.jpg" alt = "..."></a>
 								<div class = "caption" style="margin-top:-1em">             
 									<h5> Restaurant Name1</h5>
@@ -147,43 +146,58 @@
 				<!--  Personal setting  -->
 						<div class="col-md-3 col-md-3" style="margin-top:1em">
 							<div class="user-info">
-								<a href="http://localhost/menu/mn-user.php#" class="food-catagory">
-			<!-- 需要个人头像 -->
+								<a href="#" class="food-catagory">
 									<img src="../menu/image/2.jpg" class="path1">
+									<span>
+										<div class="btn-food-catagory0" align="center"><p></p><i class="icon-hamburger"></i><strong>American</strong></div>
+										<div class="btn-food-catagory1" align="center"><p></p><strong>Chinese</strong></div>
+										<div class="btn-food-catagory2" align="center"><p></p><strong>Korean</strong></div>
+										<div class="btn-food-catagory3" align="center"><p></p><strong>Japanese</strong></div>
+										<div class="btn-food-catagory4" align="center"><p></p><strong>Indian</strong></div>
+										<div class="btn-food-catagory5" align="center"><p></p><strong>More</strong></div>
+									</span>
 								</a>
 
 								<div class="btn-ice1" align="center"><p></p><strong><?php echo $_SESSION['username'];?></strong></div>
-								<a href="mn-user-account.php" class="btn-ice2" align="center" style="color:black"><p></p><i class="fa fa-cog fa=spin"></i><strong>Account</strong></a>
+								<div class="btn-ice2" align="center"><p></p><i class="fa fa-cog fa=spin"></i><strong>Account</strong></div>
 								<div class="btn-ice3" align="center"><p></p><i class="fa fa-shopping-cart"></i><strong>Cart</strong></div>
-								<div class="btn-ice4">							
+								<div class="btn-ice4">
+						<!-- day setting -->
+									<div class="day-box" style="margin-top:-17em; margin-left:10em" >
+										<h2>Nov.</h2>
+										<p></p><h3>10</h3>
+									</div>
 								</div>
 							</div>
 				<!-- wather：sunny cloudy rainy snowy starry stormy -->
 							<div class="cloudy"></div>
 						</div>
 				<!--   House   -->
-						<div class="col-md-4" style="margin-left:-0.5em; margin-top:-1em">
+						<div class="col-md-5 col-md-5" style="margin-left:-0.5em; margin-top:-1em">
 							<div class="housetop-box">	
-								<div class="cloudy" style="margin-top:-20px;margin-left:90px"></div>
 								<div id="house-box"></div>
 								<div id="house-box2"></div>
 								<div id="house-box3"></div>
 								<div class="newbee">
-								<img src="../menu/image/newbee.png" style="height:5.8em;width:14.3em;border-radius:20px">
-									<div id="time" class="day-box" style="margin-top:-135px;margin-left:58px;font-size:20px" >
-										<?php
-											echo date("M j") . "<br>";
-											echo date("l");
-										?>
-									</div>
-									
+									<img src="../menu/image/newbee.png" style="height:5.8em;width:14.3em;border-radius:20px">
 								</div>
 							</div>
 							<div class="housetop-box2">
 								<div class="house-favo-box">
-									<a href="http://localhost/menu/mn-user-favorites.php#" class="fav-list">
+									<a href="#" class="fav-list">
 										<div class="house-favo" align="center">
-											<p></p><i class="fa fa-heart" ></i><strong>Favorites</strong>
+											<p></p><i class="fa fa-heart"></i><strong>Favorites</strong>
+											<span>
+												<div class="fav-detal-box">
+													<div align="center"><h3>Favoritess</h3></div>
+													<ul>
+														<li></li>
+														<li></li>
+														<li></li>
+														<li></li>
+													</ul>
+												</div>
+											</span>
 											<div class="winleft">
 												<div class="win-line1"></div>
 												<div class="win-line2"></div>
@@ -192,7 +206,7 @@
 									</a>
 								</div>
 								<div class="house-coupon-box">
-									<a href="mn-user-coupon.php" class="coupon-list">
+									<a href="#" class="coupon-list">
 										<div class="house-coupon" align="center">
 											<p></p><i class="fa fa-calendar"></i><strong>Coupon</strong>
 											<div class="winright">
@@ -200,6 +214,17 @@
 												<div class="win-line2"></div>
 											</div>										
 										</div>
+										<span>
+											<div class="coupon-detal-box">
+												<div align="center"><h3>Coupons</h3></div>
+												<ul>
+													<li></li>
+													<li></li>
+													<li></li>
+													<li></li>
+												</ul>
+											</div>
+										</span>
 									</a>
 								</div>
 								<div id="house-box4"></div>
@@ -208,46 +233,33 @@
 								<div class="mouth"></div>
 							</div>
 						</div><!-- House end -->	
-						<div class="col-md-1">
-						    
-							<div class="parking5" align="left">
-								<span class="windmill2">
-								   	<span class="pillar">
-								   	<h1><i class="fa fa-home" style="margin-left:20px"/></i></h1>
-								   	</span>
-								   	<span class="axis">
-								    	<span class="swing"></span>
-								    	<span class="swing2"></span>
-								    	<span class="swing3"></span>
-								    </span>
-
-								</span>
-							</div>	
-						</div>
-						<div class="col-md-4" >
+						<div class="col-md-4 col-md-4" >
 								<div class="parking1" align="center">
 									<div id="parking4"></div>
 									<div id="parking2"></div>
 									<div id="parking3"></div>
-									<a href="mn-user-history.php" style="color:black"><h3><i class="fa fa-history" ></i>  Parking...</h3></a>
-											
+									<h3><i class="fa fa-history"></i>  Parking...</h3>
+									<div class="parking5" align="left">
+										<span class="windmill2">
+								         	<span class="pillar">
+								         		<h1><i class="fa fa-home" style="margin-left:0.6em"></i></h1>
+								         	</span>
+								         	<span class="axis">
+								       			<span class="swing"></span>
+								         		<span class="swing2"></span>
+								         		<span class="swing3"></span>
+								         	</span>
+								         </span>
+									</div>					
 								</div>
-			<!--  三个a标签需要浏览过的菜色图片  -->
-								<a href="http://localhost/menu/mn-restaurant-list.php"><img src="../menu/image/3.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-bottom:0.5em"></a>
-								<a href="http://localhost/menu/mn-restaurant-list.php"></a>><img src="../menu/image/4.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-left:5em; margin-bottom:0.5em"></a>
-								<a href="http://localhost/menu/mn-restaurant-list.php"></a>><img src="../menu/image/6.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-left:10em; margin-bottom:0.5em"></a>
+								<div><img src="../menu/image/3.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-bottom:0.5em"></div>
+								<div><img src="../menu/image/4.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-left:5em; margin-bottom:0.5em"></div>
+								<div><img src="../menu/image/6.jpg"  style="width:14.7em; height:7.7em; border: 0.2em solid black;border-radius:0.7em; overflow:hidden; margin-left:10em; margin-bottom:0.5em"></div>
 						</div>
 					</div>	<!-- Send row end -->
 				</div><!-- container end -->
 			</div><!-- End div -->
 		</div> <!-- End background -->
 	</div> <!-- parallax end -->
-	<script>
-		function myFunction() {
-   			var d = new DateTime();
-    		d.setTime(1332403882588);
-    		document.getElementById("time").innerHTML = d;
-		}
-	</script>
 </body>
 </html>
